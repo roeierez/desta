93db3d78e42301712d4137e599c279c13e750378
Roei Erez <roeierez@gmail.com>
2016-07-26 12:53:44 +0300

    port 80

--- api/index.js
+++ api/index.js
@@ -51,7 +51,7 @@ app.use(function(req, res, next){
 });
 
 
-server.listen(80, () => {
+server.listen(3000, () => {
   const host = server.address().address;
   const port = server.address().port;
 
