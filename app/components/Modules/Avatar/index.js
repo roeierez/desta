bc5ad7d00c39ef532685ea49d4453c87e3561fe0
Roei Erez <roeierez@gmail.com>
2016-09-04 23:13:17 +0300

    fix avatar

--- app/components/Modules/Avatar/index.js
+++ app/components/Modules/Avatar/index.js
@@ -9,6 +9,10 @@ class Avatar extends React.Component {
 
     render() {
         let {width, height, id, name} = this.props;
+        if (!name) {
+            return <img onClick={this.handleClick.bind(this)} style={{width,height, borderRadius: width/2 + 'px'}} className="avatar" src={`https://graph.facebook.com/v2.7/${id}/picture?type=small&width=${width}&height=${height}`} />;
+        }
+
         return (
             <div className="avatar-container">
                 <img onClick={this.handleClick.bind(this)} style={{width,height, borderRadius: width/2 + 'px'}} className="avatar" src={`https://graph.facebook.com/v2.7/${id}/picture?type=small&width=${width}&height=${height}`} />
