82cfbbb80c5dc09c1a018c6a1fd30cd7930498cb
Roei Erez <roeierez@gmail.com>
2016-10-26 21:23:51 +0300

    fix loader sass

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -10,7 +10,7 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.scss$/,
-      loader: ExtractTextPlugin.extract('style-loader', 'css?minimize!postcss-loader!less-loader'),
+      loader: ExtractTextPlugin.extract('style-loader', 'css?minimize!postcss-loader!sass-loader'),
     }],
   },
   output: {
